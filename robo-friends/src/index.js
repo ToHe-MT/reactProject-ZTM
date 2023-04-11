e9882933b6ddec53189e7fdd0cedfb6efe39eaf4
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-04-11 17:20:34 +0700

    Change and Import WEBBROWSER

--- robo-friends/src/index.js
+++ robo-friends/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import BrowserRouter from "react-router-dom/BrowserRouter"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="{oricess.env.PUBLIC_URL">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
