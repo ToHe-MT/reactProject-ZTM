e1608204cdda39541439ddf2cf787ef8af5da39f
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-04-11 17:44:19 +0700

    Deploy

--- robo-friends/src/index.js
+++ robo-friends/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BrowserRouter from "react-router-dom/BrowserRouter"
+import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="{oricess.env.PUBLIC_URL">
-    <React.StrictMode>
+  <BrowserRouter basename="{process.env.PUBLIC_URL}">
+    {/* <React.StrictMode> */}
       <App />
-    </React.StrictMode>
+    {/* </React.StrictMode> */}
   </BrowserRouter>
 );
 
